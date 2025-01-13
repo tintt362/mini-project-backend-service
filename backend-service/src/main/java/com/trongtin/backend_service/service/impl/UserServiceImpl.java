9c8c73af78ef0b062ebaa6b1eef5a97de77ae660
Thai Trong Tin <tintt362@gmail.com>
2025-01-13 19:53:31 +0700

    code create user and update user

--- backend-service/src/main/java/com/trongtin/backend_service/service/impl/UserServiceImpl.java
+++ backend-service/src/main/java/com/trongtin/backend_service/service/impl/UserServiceImpl.java
@@ -1,18 +1,33 @@
 package com.trongtin.backend_service.service.impl;
 
+import com.trongtin.backend_service.common.UserStatus;
 import com.trongtin.backend_service.dto.request.UserCreationRequest;
 import com.trongtin.backend_service.dto.request.UserPasswordRequest;
 import com.trongtin.backend_service.dto.request.UserUpdateRequest;
 import com.trongtin.backend_service.dto.response.UserPageResponse;
 import com.trongtin.backend_service.dto.response.UserResponse;
+import com.trongtin.backend_service.exception.ResourceNotFoundException;
+import com.trongtin.backend_service.model.AddressEntity;
+import com.trongtin.backend_service.model.UserEntity;
+import com.trongtin.backend_service.repository.AddressRepository;
+import com.trongtin.backend_service.repository.UserRepository;
 import com.trongtin.backend_service.service.UserService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
+    private final UserRepository userRepository;
+    private final AddressRepository addressRepository;
     @Override
     public UserPageResponse findAll(String keyword, String sort, int page, int size) {
         return null;
@@ -34,14 +49,87 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public long save(UserCreationRequest req) {
-        log.info("Save user: {}",req.toString());
-        return 0;
+        log.info("Saving user: {}", req);
+        UserEntity user = new UserEntity();
+        user.setFirstName(req.getFirstName());
+        user.setLastName(req.getLastName());
+        user.setGender(req.getGender());
+        user.setBirthday(req.getBirthday());
+        user.setEmail(req.getEmail());
+        user.setPhone(req.getPhone());
+        user.setUsername(req.getUsername());
+        user.setType(req.getType());
+        user.setStatus(UserStatus.NONE);
+        userRepository.save(user);
+        log.info("Saved user: {}", user);
+
+        if (user.getId() != null) {
+            log.info("user id: {}", user.getId());
+            List<AddressEntity> addresses = new ArrayList<>();
+            req.getAddresses().forEach(address -> {
+                AddressEntity addressEntity = new AddressEntity();
+                addressEntity.setApartmentNumber(address.getApartmentNumber());
+                addressEntity.setFloor(address.getFloor());
+                addressEntity.setBuilding(address.getBuilding());
+                addressEntity.setStreetNumber(address.getStreetNumber());
+                addressEntity.setStreet(address.getStreet());
+                addressEntity.setCity(address.getCity());
+                addressEntity.setCountry(address.getCountry());
+                addressEntity.setAddressType(address.getAddressType());
+                addressEntity.setUserId(user.getId());
+                addresses.add(addressEntity);
+            });
+            addressRepository.saveAll(addresses);
+            log.info("Saved addresses: {}", addresses);
+        }
+
+        return user.getId();
     }
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public void update(UserUpdateRequest req) {
+        log.info("Updating user: {}", req);
 
+        // Get user by id
+        UserEntity user = getUserEntity(req.getId());
+        user.setFirstName(req.getFirstName());
+        user.setLastName(req.getLastName());
+        user.setGender(req.getGender());
+        user.setBirthday(req.getBirthday());
+        user.setEmail(req.getEmail());
+        user.setPhone(req.getPhone());
+        user.setUsername(req.getUsername());
+
+        userRepository.save(user);
+        log.info("Updated user: {}", user);
+
+        // save address
+        List<AddressEntity> addresses = new ArrayList<>();
+
+        req.getAddresses().forEach(address -> {
+            AddressEntity addressEntity = addressRepository.findByUserIdAndAddressType(user.getId(), address.getAddressType());
+            if (addressEntity == null) {
+                addressEntity = new AddressEntity();
+            }
+            addressEntity.setApartmentNumber(address.getApartmentNumber());
+            addressEntity.setFloor(address.getFloor());
+            addressEntity.setBuilding(address.getBuilding());
+            addressEntity.setStreetNumber(address.getStreetNumber());
+            addressEntity.setStreet(address.getStreet());
+            addressEntity.setCity(address.getCity());
+            addressEntity.setCountry(address.getCountry());
+            addressEntity.setAddressType(address.getAddressType());
+            addressEntity.setUserId(user.getId());
+
+            addresses.add(addressEntity);
+        });
+
+        // save addresses
+        addressRepository.saveAll(addresses);
+        log.info("Updated addresses: {}", addresses);
     }
 
     @Override
@@ -52,5 +140,32 @@ public class UserServiceImpl implements UserService {
     @Override
     public void delete(Long id) {
 
+    }
+    private UserEntity getUserEntity(Long id) {
+        return userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("User not found"));
+    }
+    private static UserPageResponse getUserPageResponse(int page, int size, Page<UserEntity> userEntities) {
+        log.info("Convert User Entity Page");
+
+        List<UserResponse> userList = userEntities.stream().map(entity -> UserResponse.builder()
+                .id(entity.getId())
+                .firstName(entity.getFirstName())
+                .lastName(entity.getLastName())
+                .gender(entity.getGender())
+                .birthday(entity.getBirthday())
+                .username(entity.getUsername())
+                .phone(entity.getPhone())
+                .email(entity.getEmail())
+                .build()
+        ).toList();
+
+        UserPageResponse response = new UserPageResponse();
+        response.setPageNumber(page);
+        response.setPageSize(size);
+        response.setTotalElements(userEntities.getTotalElements());
+        response.setTotalPages(userEntities.getTotalPages());
+        response.setUsers(userList);
+
+        return response;
     }
 }
